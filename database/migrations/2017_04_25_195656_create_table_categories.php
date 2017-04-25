159ad0642649700f8926129c52d5a89063b48938
Ivan Vitali <webcoder.ivan@gmail.com>
2017-04-25 17:03:05 -0300

    dev#6 se creo la base de datos categories para las categorias

---
+++ database/migrations/2017_04_25_195656_create_table_categories.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create categories table
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Delete if exists table categories
+        Schema::dropIfExists('categories');
+    }
+}
