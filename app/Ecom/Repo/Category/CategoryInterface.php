792a64d416c17c35874cb234d91b6243e6f6014a
Ivan Vitali <webcoder.ivan@gmail.com>
2017-04-25 17:19:06 -0300

    dev#9 se creo la CategoryInterface

---
+++ app/Ecom/Repo/Category/CategoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: web
+ * Date: 25/4/17
+ * Time: 17:16
+ */
+
+    interface Category {
+
+        /**
+         * Get all categories
+         *
+         * @return mixed
+         */
+        public function getAllCategories();
+
+    }
