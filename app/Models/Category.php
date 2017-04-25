69a5cadfb86f5b754947256d826f07099ca93761
Ivan Vitali <webcoder.ivan@gmail.com>
2017-04-25 17:09:46 -0300

    dev#7 se creo la tabla categories

---
+++ app/Models/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title'
+    ];
+
+    /**
+     * Set Title attribute
+     *
+     * @param string
+     */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['firstName'] = ucfirst(strtolower($value));
+    }
+
+}
